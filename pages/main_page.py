dab9af1cf80515d70f7a4ad17e4b3488bc2b2642
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-14 21:37:27 +0300

    fields are added

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,10 @@ class Main_page(Base):
         super().__init__(driver)
 
 
+    # x-path locators:
+
+    enter_button = "//*[@id='i-5-bitrix-system-auth-form-panel-iIjGFB3HxHmm']/div"
+    pop_up = '//*[@id="UniverseComponent"]'
+    log_in_field = '//*[@id="USER_LOGIN_2"]'
+    password_field = '//*[@id="USER_PASSWORD_2"]'
+    login_popup_btn = '//*[@id="i-1-bitrix-system-auth-authorize-popup-2-0weid03RvzKY"]/form/div[3]/input'
