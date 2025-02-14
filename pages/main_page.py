14fca7945ee55b0291d2ce859825ef42007e513b
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-14 21:56:28 +0300

    getters were added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,44 @@
 from base.base import Base
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Main_page(Base):
     def __init__(self, driver):
         super().__init__(driver)
-
-
+        self.wait = WebDriverWait(self.driver, 10, poll_frequency=1)
     # x-path locators:
 
-    enter_button = "//*[@id='i-5-bitrix-system-auth-form-panel-iIjGFB3HxHmm']/div"
-    pop_up = '//*[@id="UniverseComponent"]'
-    log_in_field = '//*[@id="USER_LOGIN_2"]'
-    password_field = '//*[@id="USER_PASSWORD_2"]'
-    login_popup_btn = '//*[@id="i-1-bitrix-system-auth-authorize-popup-2-0weid03RvzKY"]/form/div[3]/input'
+    enter_button = ("xpath","//*[@id='i-5-bitrix-system-auth-form-panel-iIjGFB3HxHmm']/div")
+    pop_up =  ("xpath", '//*[@id="UniverseComponent"]')
+    log_in_field = ("xpath", '//*[@id="USER_LOGIN_2"]')
+    password_field = ("xpath", '//*[@id="USER_PASSWORD_2"]')
+    login_popup_btn = ("xpath", '//*[@id="i-1-bitrix-system-auth-authorize-popup-2-0weid03RvzKY"]/form/div[3]/input')
+
+
+    # getters
+
+    def get_enter_btn(self):
+        return self.wait.until(EC.element_to_be_clickable(self.enter_button))
+
+
+    def get_pop_up(self):
+        return  self.wait.until(EC.visibility_of_element_located(self.pop_up))
+
+
+    def get_log_in_field(self):
+        return self.wait.until(EC.element_to_be_clickable(self.log_in_field))
+
+
+    def get_password_field(self):
+        return self.wait.until(EC.element_to_be_clickable(self.password_field))
+
+
+    def get_login_popup_btn(self):
+        return self.wait.until(EC.element_to_be_clickable(self.login_popup_btn))
+
+
+
+    # actions
+
+
+
