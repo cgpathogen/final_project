67c5e3518a69e6d86e7ee1e21ac4a312087b2fe8
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-15 08:58:15 +0300

    navbar locators were added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,13 @@
 from base.base import Base
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 class Main_page(Base):
     def __init__(self, driver):
         super().__init__(driver)
         self.wait = WebDriverWait(self.driver, 10, poll_frequency=1)
+        self.action = ActionChains(self.driver)
 
 
     # main url:
@@ -25,6 +27,8 @@ class Main_page(Base):
     log_in_field = ("xpath", '//*[@id="USER_LOGIN_2"]')
     password_field = ("xpath", '//*[@id="USER_PASSWORD_2"]')
     login_popup_btn = ("xpath", '//*[@id="i-1-bitrix-system-auth-authorize-popup-2-0weid03RvzKY"]/form/div[3]/input')
+    catalog = ("xpath", '//*[@id="i-8-bitrix-menu-horizontal-1-XEVOpkwAkIZ0"]/div[2]/div/div/div[1]/div[1]/a')
+    catalog_sublink = ("xpath", '//*[@id="i-8-bitrix-menu-horizontal-1-XEVOpkwAkIZ0"]/div[2]/div/div/div[1]/div[1]/a/div[2]')
 
 
     # getters
@@ -49,6 +53,13 @@ class Main_page(Base):
         return self.wait.until(EC.element_to_be_clickable(self.login_popup_btn))
 
 
+    def get_catalog_link(self):
+        return self.wait.until(EC.visibility_of_element_located(self.catalog))
+
+    def get_catalog_sublink(self):
+        return self.wait.until(EC.visibility_of_element_located(self.catalog_sublink))
+
+
     # actions
 
 
@@ -70,6 +81,10 @@ class Main_page(Base):
     def click_login_button(self):
         self.get_login_popup_btn().click()
         print("authorization passed")
+
+
+    def hover_catalog(self):
+        self.action.move_to_element(self.get_catalog_link()).click(self.get_catalog_sublink()).perform()
 
 
     # methods:
