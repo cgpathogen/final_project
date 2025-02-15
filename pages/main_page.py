b5e319b1687b3ae1f8b0c818cff877b9d81d2fea
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-15 13:28:33 +0300

    self.choose_user_location() was added to authorization method

--- pages/main_page.py
+++ pages/main_page.py
@@ -121,8 +121,8 @@ class Main_page(Base):
 
     def authorization(self):
         self.driver.get(url=self.main_url)
+        self.choose_user_location()
         self.get_current_url()
-        self.hide_location_popup()
         self.click_enter_button()
         self.send_login()
         self.send_password()
