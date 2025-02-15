328dfa4e49a3cb73ab0e67de1e94132760471702
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-15 18:06:21 +0300

    go_to_main_url imported from main_page.py

--- base/base.py
+++ base/base.py
@@ -8,6 +8,11 @@ class Base:
         self.wait = WebDriverWait(self.driver,10,poll_frequency=1)
         self.action = ActionChains(self.driver)
 
+    main_url = "https://legendbaikal.ru/"
+
+    def go_to_main_url(self):
+        self.driver.get(url=self.main_url)
+
 
     def get_current_url(self):
         print(f"\nCurrent URL: {self.driver.current_url}")
