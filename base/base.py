67a5470dc27435eb51aec68c33ae5589078e6f65
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-15 13:55:09 +0300

    popup methods transported to main_page.py

--- base/base.py
+++ base/base.py
@@ -9,44 +9,8 @@ class Base:
         self.action = ActionChains(self.driver)
 
 
-    # locators
-
-    location_popup = ("xpath",'//*[@id="popup-window-content-i-4-intec-regionality-regions-select-template-1-b7WAH1g6DqWX-question"]/div')
-    popup_close_button = ("xpath",'//*[@id="popup-window-content-i-4-intec-regionality-regions-select-template-1-b7WAH1g6DqWX-question"]/div/div[3]')
-    pcb_yes = ("xpath",'//button[@class="regions-select-question-button intec-cl-background intec-cl-background-light-hover"]')
-    pcb_no = ("xpath", '//button[@class="regions-select-question-button"]')
-
-
-    # getters
-
-
-    def get_location_popup(self):
-        return self.wait.until(EC.presence_of_element_located(self.location_popup))
-
-
-    def get_popup_close_button(self):
-        return self.wait.until(EC.element_to_be_clickable(self.popup_close_button))
-
-
-    def get_pcb_yes(self):
-        return self.wait.until(EC.element_to_be_clickable(self.pcb_yes))
-
-
-    def get_pcb_no(self):
-        return self.wait.until(EC.element_to_be_clickable(self.pcb_no))
-
-
-    # actions
-
-
     def get_current_url(self):
         print(f"\nCurrent URL: {self.driver.current_url}")
 
 
-    def hide_location_popup(self):
-        location_popup = self.wait.until(EC.presence_of_element_located(self.location_popup))
-        self.action.move_to_element(location_popup).perform()
 
-        close_button = self.wait.until(EC.element_to_be_clickable(self.get_popup_close_button()))
-        close_button.click()
-        print("Location choosing pop-up was closed")
