0e0316f50387b34d5a596b3e514c0c4de9253222
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-18 21:55:29 +0300

    xpath locators were added

---
+++ pages/cart_page.py
@@ -0,0 +1,28 @@
+import time
+
+from base.base import Base
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+from pages.catalogue import Catalogue
+
+
+
+class Cart_page(Base):
+    def __init__(self,driver):
+        super().__init__(driver)
+        self.wait = WebDriverWait(self.driver, 10, poll_frequency=1)
+        self.action = ActionChains(self.driver)
+
+
+    # xpath locators
+
+    added_cart_item_name = ("xpath","//a[@class='intec-cl-text-hover']")
+    added_cart_item_2_name = ("xpath", "(//a[@class='intec-cl-text-hover'])[2]")
+
+    added_cart_item_price = ("xpath", "//div[@class='basket-item-total-value']")
+    added_cart_item_price_2 = ("xpath", "(//div[@class='basket-item-total-value'])[2]")
+
+    added_item_count = ("xpath","//input[@class='intec-ui-part-input']")
+    added_item_2_count = ("xpath", "//input[@class='intec-ui-part-input']")
