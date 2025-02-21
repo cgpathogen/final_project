fb3b4f1ac0018a5484019b23a79024b0c6f45ef6
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-21 23:01:56 +0300

    xpaths and getters returned to the file

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -14,11 +14,42 @@ class Cart_page(Base):
 
     # xpath locators
 
+    added_cart_item_name = ("xpath","(//a[@class='intec-cl-text-hover'])[1]")
+    added_cart_item_2_name = ("xpath", "(//a[@class='intec-cl-text-hover'])[2]")
+
+    added_cart_item_price = ("xpath", "(//div[@class='basket-item-total-value'])[1]")
+    added_cart_item_price_2 = ("xpath", "(//div[@class='basket-item-total-value'])[2]")
+
+    added_item_count = ("xpath","(//input[@class='intec-ui-part-input'])[1]")
+    added_item_2_count = ("xpath", "(//input[@class='intec-ui-part-input'])[2]")
 
     place_order_btn = ("xpath","//button[@class='basket-order-button intec-ui intec-ui-control-button intec-ui-mod-round-2 intec-ui-scheme-current intec-ui-mod-block intec-ui-size-2']")
 
 
     # getters
+
+    def get_added_cart_item_name(self):
+        return self.wait.until(EC.visibility_of_element_located(self.added_cart_item_name))
+
+
+    def get_added_cart_item_2_name(self):
+        return self.wait.until(EC.visibility_of_element_located(self.added_cart_item_2_name))
+
+
+    def get_added_cart_item_price(self):
+        return self.wait.until(EC.visibility_of_element_located(self.added_cart_item_price))
+
+
+    def get_added_cart_item_price_2(self):
+        return self.wait.until(EC.visibility_of_element_located(self.added_cart_item_price_2))
+
+
+    def get_added_item_count(self):
+        return self.wait.until(EC.element_to_be_clickable(self.added_item_count))
+
+
+    def get_added_item_2_count(self):
+        return self.wait.until(EC.element_to_be_clickable(self.added_item_2_count))
 
 
     def get_place_order_btn(self):
