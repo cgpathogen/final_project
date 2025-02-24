46f64986a7aeb938d00b6fdb9289734297350d38
anatoly_bobrov <acrocanthbm@gmail.com>
2025-02-24 21:49:45 +0300

    driver moved to fixture

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,24 +1,14 @@
 import time
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-
 from pages.main_page import Main_page
 from pages.catalogue import Catalogue
 from pages.cart_page import Cart_page
 from pages.place_order import Place_order
 
-def test_buy_product():
-    options = Options()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--useragent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(options=options,service=service)
+from conftest import init_driver
 
+def test_buy_product(init_driver):
+    driver = init_driver
     mp = Main_page(driver) # main page
     mp.go_to_main_url()
     mp.close_location_popup()
